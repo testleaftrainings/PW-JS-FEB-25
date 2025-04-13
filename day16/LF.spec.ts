619a4ea7ee346f36d748def88c5ca73d29787179
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-04-13 14:05:34 +0530

    Delete day16 directory

--- day16/LF.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from "@playwright/test";
-
-test(`Learn custom retry`,async({page})=>{
- await page.goto("http://leaftaps.com/opentaps/control/main")
- await page.fill("#username","demoCSR")
- await page.fill("#password","crmsfa")
- await page.cickAnddelay(".decorativeSubmit")
-})
--- day16/customretry.d.ts
+++
@@ -1,8 +0,0 @@
-import {Page} from '@playwright/test'
-
-declare module '@playwright/test'{
-    interface Page{
-        cickAnddelay:(locator:string)=>Promise<void>
-        delayedType:(locator:string,data:string)=>Promise<void>
-    }
-}
