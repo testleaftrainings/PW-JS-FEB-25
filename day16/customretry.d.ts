5d2653260af419072bd55584c113f274eb25ca83
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-04-13 14:01:57 +0530

    Add files via upload

---
+++ day16/customretry.d.ts
@@ -0,0 +1,8 @@
+import {Page} from '@playwright/test'
+
+declare module '@playwright/test'{
+    interface Page{
+        cickAnddelay:(locator:string)=>Promise<void>
+        delayedType:(locator:string,data:string)=>Promise<void>
+    }
+}
